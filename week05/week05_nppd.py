2b9443c3d959b6a5891766dddd03fab3efd05a7f
202030310 <dkdtjdgh35@gmail.com>
2024-09-19 12:49:50 +0900

    v1.2 BP2

--- week05/week05_nppd.py
+++ week05/week05_nppd.py
@@ -1,22 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+#from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+
 
 data_root = "https://github.com/ageron/data/raw/main/"
 lifesat = pd.read_csv(data_root + "lifesat/lifesat.csv")
 X = lifesat[["GDP per capita (USD)"]].values
 y = lifesat[["Life satisfaction"]].values
 
-print(lifesat.head(27))
+#print(lifesat.head(27))
 
-# lifesat.plot(kind='scatter', grid=True, x="GDP per capita (USD)", y="Life satisfaction")
-# plt.axis([23_500, 62_500, 4, 9])
-# plt.show()
-#
-# model = LinearRegression()
-#
-# model.fit(X, y)
-#
-# X_new = [[32_142.2]] #
-# print(model.predict(X_new))
+lifesat.plot(kind='scatter', grid=True, x="GDP per capita (USD)", y="Life satisfaction")
+plt.axis([23_500, 62_500, 4, 9])
+
+plt.show()
+
+#model = LinearRegression()
+model = KNeighborsRegressor(n_neighbors=3) #한국과 1인당 GDP가 가장 가까운 나라 셋
+
+model.fit(X, y)
+
+X_new = [[32_142.2]] #
+print(model.predict(X_new))
