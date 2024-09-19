ece5dd0b548680481b4c8067906cb5da8dc66679
202030310 <dkdtjdgh35@gmail.com>
2024-09-19 10:25:05 +0900

    v0.8 BP2

--- week04/week04.web01.py
+++ week04/week04.web01.py
@@ -1,11 +1,18 @@
 import urllib.request
 import urllib.parse
 
-api = 'https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp'
+api = '//www.kma.go.kr/weather/forecast/mid-term-rss3.jsp'
 
 station_id = input("지역코드 : ") # 전국 108, 수도권 109, 강원 105, 제주 184...
 values = {'stnId' : station_id}
 
-url = api + '?' + str(values)
+url = api + '?' + urllib.parse.urlencode(values)
 print(url)
 
+"""
+지역코드 : 184
+//www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?{'stnId': '184'}
+//www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=184
+
+Process finished with exit code 0
+"""
