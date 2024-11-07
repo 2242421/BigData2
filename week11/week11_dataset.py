06e5a5003c589b0a9ac88fea40d2b6efbdc09e72
202030310 <dkdtjdgh35@gmail.com>
2024-11-07 10:49:59 +0900

    v1.9 BP2

---
+++ week11/week11_dataset.py
@@ -0,0 +1,34 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# 성별에 따른 생존율 계산
+titanic = sns.load_dataset('titanic')
+#print(titanic['sex'].head())
+#print(titanic.info())
+gender_survival = titanic.groupby(by='sex')['survived'].mean()
+print(gender_survival)
+
+
+
+
+
+
+
+
+
+#titanic['deck'] = titanic['deck'].cat.add_categories('Unknown')
+#titanic['deck'].fillna('Unknown', inplace=True)
+#print(titanic['deck'])
+#print(titanic.info())
+
+#print(titanic['survived'])
+#print(titanic.info())
+
+
+# 생존자 수와 사망자 수 시각화
+sns.countplot(data=titanic, x='survived')
+plt.title('Survived (0 = NO, 1 = YES)')
+plt.xlabel('Survived')
+plt.ylabel('Count')
+#plt.show()
